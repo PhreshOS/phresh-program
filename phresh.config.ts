19659349e8dd1fb123bc6559cd2f103010b00736
zohayrslileh <zohayrslileh@gmail.com>
2026-08-28 16:26:26 +0100

    Simplify client development command

--- phresh.config.ts
+++ phresh.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         size: { width: 600, height: 500 },
         development: {
             url: "http://localhost:5200/",
-            startCommand: "vite dev --config vite.client.ts"
+            startCommand: "vite --config vite.client.ts"
         }
     }
 })
