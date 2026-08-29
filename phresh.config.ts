010ba52f8d39d140dc6f72e6d0d773f19913d9d8
zohayrslileh <zohayrslileh@gmail.com>
2026-08-29 03:37:14 +0100

    Release Phresh 0.1.19

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     identity: "phresh",
     name: "Phresh Program",
     description: "A minimal counter with direct Client and Server endpoints.",
-    version: "0.1.18",
+    version: "0.1.19",
     icon: "icon.png",
     categories: ["Development"],
     keywords: ["example", "counter", "client", "server"],
