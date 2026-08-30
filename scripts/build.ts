2e7f75e1ad8ff8a41319ee56f08bfa1bebe316cf
zohayrslileh <zohayrslileh@gmail.com>
2026-08-30 17:28:49 +0100

    Support external Server dependencies

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,16 @@
+import { externalDependencies } from "../vite.config"
 import { rm, writeFile } from "node:fs/promises"
+import packageConfig from "../package.json" with { type: "json" }
 
 process.env.NODE_ENV = "production"
 
 const { build } = await import("vite")
+const dependencies: Partial<typeof packageConfig.dependencies> = {}
 
 await rm("dist", { recursive: true, force: true })
+
+for (const dependency of externalDependencies) dependencies[dependency] = packageConfig.dependencies[dependency]
+
 await build({ configFile: "vite.config.ts", ssr: { noExternal: true } })
 await build({ configFile: "vite.client.ts" })
-await writeFile("dist/server/package.json", JSON.stringify({ type: "module" }))
+await writeFile("dist/server/package.json", JSON.stringify({ type: "module", dependencies }))
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import packageConfig from "./package.json" with { type: "json" }
 import { defineConfig } from "vite"
 import { resolve } from "node:path"
 
+export const externalDependencies: (keyof typeof packageConfig.dependencies)[] = []
+
 export default defineConfig({
     root: "server",
+    ssr: {
+        external: externalDependencies
+    },
     build: {
         ssr: true,
         emptyOutDir: true,
